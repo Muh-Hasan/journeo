42ac533907e5a6af0b7717d5f8bd196965f5f282
Muhammad Aqib Shah <109861813+Muh-Aqib-Shah@users.noreply.github.com>
2024-07-29 10:38:53 +0000

    feat:added explore-card and explore-trips page

---
+++ src/app/explore/page.tsx
@@ -0,0 +1,41 @@
+import TripCard from '@/components/explore/explore-card';
+import SearchBar from '@/components/explore/searchBar';
+
+export default function Explore() {
+  const dummyData = Array(6).fill({
+    img: '/placeholder_location.png',
+    title: 'Exclusice Seoul 8-day Trip',
+    desc: 'enjoy a beautiful vacation in the vibrant city of seoul, get lost in the culture of blossom',
+    providerName: 'Chris Nolan',
+    duration: 7,
+    likes: 20,
+  });
+
+  return (
+    <div className="container mt-12 space-y-4">
+      <div className="flex flex-col items-center justify-center space-y-2">
+        <h2 className="text-2xl font-semibold">Explore Trips</h2>
+        <div className="text-center">
+          Find trips created by other users and get inspired for your next
+          vacation
+        </div>
+        <SearchBar />
+      </div>
+
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {dummyData &&
+          dummyData.map((elem) => (
+            <TripCard
+              img={elem.img}
+              title={elem.title}
+              desc={elem.desc}
+              providerName={elem.providerName}
+              duration={elem.duration}
+              likes={elem.likes}
+              key={elem.title}
+            />
+          ))}
+      </div>
+    </div>
+  );
+}
